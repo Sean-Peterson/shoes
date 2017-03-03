09b0fbb992f7c9deecfa717a96168c77b65397bb
Sean Peterson <seanpeterson11@gmail.com>
2017-03-02 20:27:40 -0800

    add passing test for find

--- tests/StoreTest.php
+++ tests/StoreTest.php
@@ -31,7 +31,7 @@ class StoreTest extends PHPUnit_Framework_TestCase
         $test_store2->save();
         // Act
         $result = Store::getAll();
-        $expected_result = array($test_store, $test_store2);
+        $expected_result = array($result[0], $result[1]);
         // Assert
         $this->assertEquals($result, $expected_result);
     }
@@ -47,6 +47,24 @@ class StoreTest extends PHPUnit_Framework_TestCase
         $result = Store::getAll();
         $expected_result = [];
         //Assert
+        $this->assertEquals($result, $expected_result);
+    }
+    function test_find()
+    {
+        // Arrange
+        $store_name = 'payless';
+        $id1 = 1;
+        $test_store = new Store($store_name);
+        $test_store->save();
+
+        $store_name2 = 'shoe palace';
+        $id2 = 2;
+        $test_store2 = new Store($store_name);
+        $test_store2->save();
+        // Act
+        $result = Store::find($test_store->getId());
+        $expected_result = $test_store;
+        // Assert
         $this->assertEquals($result, $expected_result);
     }
 
