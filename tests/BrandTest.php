06f4bcedbccf1b0638ffe1bbc46a05f398d24833
Sean Peterson <seanpeterson11@gmail.com>
2017-03-08 19:56:08 -0800

    add tests for addBrand getBrands, getStores

--- tests/BrandTest.php
+++ tests/BrandTest.php
@@ -5,6 +5,7 @@
 * @backupStaticAttributes disabled
 */
 
+require_once "src/Store.php";
 require_once "src/Brand.php";
 
 $server = 'mysql:host=localhost:3306;dbname=shoes_test';
@@ -78,6 +79,37 @@ class BrandTest extends PHPUnit_Framework_TestCase
         // Act
         $result = Brand::find($test_brand->getId());
         $expected_result = $test_brand;
+        // Assert
+        $this->assertEquals($result, $expected_result);
+    }
+
+    function testGetStores()
+    {
+        // Arrange
+        $brand_name = 'starburry';
+        $test_brand = new Brand($brand_name);
+        $test_brand->save();
+
+        $brand_name2 = 'shaq';
+        $test_brand2 = new Brand($brand_name);
+        $test_brand2->save();
+
+        $store_name = 'payless';
+        $test_store = new Store($store_name);
+        $test_store->save();
+
+        $store_name2 = 'shoe palace';
+        $test_store2 = new Store($store_name);
+        $test_store2->save();
+
+        $test_store->addBrand($test_brand->getId());
+        $test_store->addBrand($test_brand2->getId());
+        $test_store2->addBrand($test_brand->getId());
+        $test_store2->addBrand($test_brand2->getId());
+
+        // Act
+        $result = $test_brand->getStores();
+        $expected_result = [$test_store, $test_store2];
         // Assert
         $this->assertEquals($result, $expected_result);
     }
--- tests/StoreTest.php
+++ tests/StoreTest.php
@@ -5,6 +5,7 @@
 * @backupStaticAttributes disabled
 */
 
+require_once "src/Brand.php";
 require_once "src/Store.php";
 
 $server = 'mysql:host=localhost:3306;dbname=shoes_test';
@@ -97,6 +98,37 @@ class StoreTest extends PHPUnit_Framework_TestCase
         $test_store->updateStoreName('Frederick');
         $result = $test_store->getStoreName();
         $expected_result = 'Frederick';
+        // Assert
+        $this->assertEquals($result, $expected_result);
+    }
+
+    function testAddBrandGetBrands()
+    {
+        // Arrange
+        $brand_name = 'starburry';
+        $test_brand = new Brand($brand_name);
+        $test_brand->save();
+
+        $brand_name2 = 'shaq';
+        $test_brand2 = new Brand($brand_name);
+        $test_brand2->save();
+
+        $store_name = 'payless';
+        $test_store = new Store($store_name);
+        $test_store->save();
+
+        $store_name2 = 'shoe palace';
+        $test_store2 = new Store($store_name);
+        $test_store2->save();
+
+        $test_store->addBrand($test_brand->getId());
+        $test_store->addBrand($test_brand2->getId());
+        $test_store2->addBrand($test_brand->getId());
+        $test_store2->addBrand($test_brand2->getId());
+
+        // Act
+        $result = $test_store->getBrands();
+        $expected_result = [$test_brand, $test_brand2];
         // Assert
         $this->assertEquals($result, $expected_result);
     }
